9b5ecb8a3ae4ace762027eab35eb750546920a08
LaurentDeus <laurentdeusdedith75@gmail.com>
2025-12-14 00:15:41 +0300

    Added getMemberID method

--- Member.java
+++ Member.java
@@ -1,9 +1,23 @@
 class Member {
 	
+	private static int members;
 	private int memberId;
 	private String fullname;
 	private String unit;
-	private static final int monthlyContribution = 10000;
+	private int monthlyContribution;
 	private double totalPaid;
 	
+	public Member(String fullname, String unit){
+		members ++;
+		this.fullname = fullname;
+		this.unit = unit;
+		memberId = members;
+		monthlyContribution = 10000;
+		totalPaid = 0;
+	}
+	
+	public int getMemberId(){
+		
+		return this.memberId;
+	}
 }
